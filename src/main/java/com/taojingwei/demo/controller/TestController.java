177f91ddbac1c6a05f3a2a65979acfe6fd4cf749
liuwei <1249074319@qq.com>
2021-04-18 16:31:26 +0800

    taojingwei

--- src/main/java/com/taojingwei/demo/controller/TestController.java
+++ src/main/java/com/taojingwei/demo/controller/TestController.java
@@ -2,7 +2,6 @@ package com.taojingwei.demo.controller;
 
 import com.taojingwei.demo.pojo.User;
 import com.taojingwei.demo.service.IUserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
